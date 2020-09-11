cd933ae71cba88813821693b17f025b9cd25416f
Ben Thomasson <bthomass@redhat.com>
2020-09-11 17:57:36 -0400

    Adds tests for rules

---
+++ ansible_state/util.py
@@ -0,0 +1,13 @@
+
+# Escape the pattern with a limited set of regexp special characters
+# To allow some of the regexps to be used.
+# This might now work out in the long term
+# We might want to use glob pattern matching or something simple like that
+_special_chars_map = {i: '\\' + chr(i) for i in b'()[]{}-|^$.&~# \t\n\r\v\f'}
+
+def escape(pattern):
+    return pattern.translate(_special_chars_map)
+
+def make_matcher(pattern):
+    pattern = escape(pattern)
+    return f'^({pattern}).*'
--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,6 +1,11 @@
 
+from deepdiff import DeepDiff, extract
+
+import re
 
 import yaml
+
+from ansible_state.util import make_matcher
 
 def test_rule1():
 
@@ -34,8 +39,37 @@ def test_rule2():
 
 def test_rules():
 
+    t1 = yaml.safe_load('''
+    routers:
+        - name: R1
+        - name: R2
+    ''')
+
+    t2 = yaml.safe_load('''
+    routers:
+        - name: R1
+        - name: R3
+    ''')
+
     rules = yaml.safe_load(r'''
                            rules:
                             - rule_selector: root['routers'][\d+]
                               inventory_selector: name
                            ''')
+
+    diff = DeepDiff(t1, t2)
+    assert re.match(make_matcher(r"root['routers'][\d+]"), "root['routers'][1]['name']")
+
+
+    for rule in rules['rules']:
+        matcher = make_matcher(rule['rule_selector'])
+        changed_path = list(diff['values_changed'].keys())[0]
+        match =  re.match(matcher, changed_path)
+        assert match
+        changed_subtree_path = match.groups()[0] 
+        assert changed_subtree_path == "root['routers'][1]"
+        changed_value = extract(t2, changed_path)
+        assert changed_value == 'R3'
+        changed_subtree = extract(t2, changed_subtree_path)
+        assert changed_subtree == {'name': 'R3'}
+
